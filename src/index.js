53814e4e5f78791b4830983153c33aaae0325027
Mihir Okte <mihirokte77@gmail.com>
2021-03-30 01:12:52 +0530

    Change 3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Senpai from './personal';
 
 const routs = (
-  <BrowserRouter >
+  <HashRouter >
      <Switch>
         <Route exact path="/"><App /></Route>
         <Route path="/senpaimode"><Senpai /></Route>
      </Switch>
-  </ BrowserRouter >
+  </ HashRouter >
 );
 
 ReactDOM.render(routs, document.getElementById('root'))
